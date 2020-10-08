9aa966bfbaa6c2c962d77084ead8bb7c569ff29f
Kyle Barthelmes <kbart2401@gmail.com>
2020-10-08 13:16:04 -0400

    passing phase 2

--- mole.js
+++ mole.js
@@ -1,1 +1,8 @@
-// Write your JS here.
+function popUpRandomMole() {
+    let heads = document.querySelectorAll('.wgs__mole-head');
+    const random = Math.floor(Math.random() * 7.9);
+    let currHead = heads[random];
+    currHead.classList.remove('wgs__mole-head--hidden')
+    setTimeout(hideMole, 1000);
+}
+
