e232242b7e5782882fcab42a9adc13b58e373ac7
Kyle B <Kbart2401@gmail.com>
2020-10-09 10:46:25 -0400

    Has a window prompt to enter name

--- mole.js
+++ mole.js
@@ -2,27 +2,39 @@ window.addEventListener("DOMContentLoaded", () => {
     let heads = document.querySelectorAll('.wgs__mole-head');
     let count = 30;
     let score = 0;
+
     setTimeout(popUpRandomMole, 0);
 
+    /************FUNCTIONS*************/
     function popUpRandomMole() {
         count--;
         document.getElementById('moles').innerHTML = `Moles ${count}`;
         if (count === 0) {
             let end = document.getElementById("game-over");
-            end.innerHTML = "Game Over";
+            end.innerHTML = "Game Over!";
             return;
         }
-        const random = Math.floor(Math.random() * 7.9);
+        const random = Math.floor(Math.random() * 15.9);
         let currHead = heads[random];
         if (currHead === undefined) popUpRandomMole();
         currHead.classList.remove('wgs__mole-head--hidden')
-        setTimeout(hideMole, 1000, currHead);
+        setTimeout(hideMole, 900, currHead);
     }
 
     function hideMole(ele) {
         ele.classList.add('wgs__mole-head--hidden');
         setTimeout(popUpRandomMole, 1000);
     }
+
+
+    /**********EVENT LISTENERS*************/
+    document.querySelector('.form-input')
+        .addEventListener('submit', event => {
+            document.querySelector('.form')
+                .classList.add('hidden');
+                event.preventDefault();
+        })
+
 
     Array.from(heads).forEach((head) => {
         head.addEventListener("click", event => {
